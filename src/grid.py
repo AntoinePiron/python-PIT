7238e1d936eab85160a8b8debb872f1385c0e77e
AntoinePiron <pro.piron.antoine@gmail.com>
2021-09-26 23:19:36 +0200

    avancement play_sudoku

--- src/grid.py
+++ src/grid.py
@@ -1,6 +1,5 @@
 #-*-coding: utf8-*-
 import linecache
-from os import replace
 
 class SudokuGrid:
 
--- src/play_sudoku.py
+++ src/play_sudoku.py
@@ -1,19 +1,51 @@
+#!/usr/bin/env python3
+#-*-coding: utf8-*-
+
 from grid import SudokuGrid
 import sys
 import os
 
-#Méthode qui sera call au début, et qui, en fonction des argument donnés en ligne de commande, crée directement 
-#une grille à partir d'une fichier valdie
+
+def play(game_grid):
+    while len(game_grid.get_empty_pos()) > 0:
+        print("Etat actuel de la grille : ")
+        print("--------------------------------------")
+        print(game_grid)
+        print("--------------------------------------")
+        while 1:
+            x = input("x : ")
+            y = input("y : ")
+            v = input("v : ")
+            try:
+                x = int(x)
+                y = int(y)
+                v = int(v)
+            except:
+                print("Veuillez renseigner des entier.")
+                continue
+            if x > 8 or x < 0 or y < 0 or y > 8 or v < 1 or v > 9:
+                print("Veuillez renseigner des valeurs compatibles.")
+                continue
+            else:
+                game_grid.write(x, y, v)
+                break
+
+
 def newGame():
-    #Si le seul argument est le nom du programme
+    # Si le seul argument est le nom du programme
     if len(sys.argv) == 1:
-        SudokuGrid.from_stdin()
-    elif len(sys.argv) == 2 or len(sys.argv) > 3 : 
-        print("Entrez 2 argument svp, un nom de fichier et un numéro de ligne")
+        play(SudokuGrid.from_stdin())
+    elif len(sys.argv) == 2 or len(sys.argv) > 3:
+        print("mauvais nb arg")
     else:
-        if os.path.isfile(sys.argv[2]) and type(sys.argv[3]) == int:
-            SudokuGrid.from_file(sys.argv[2],sys.argv[3])
-        else:
-            print("Argument non valables, réessayez")
- 
+        try:
+            num = int(sys.argv[2])
+            if os.path.isfile(sys.argv[1]):
+                play(SudokuGrid.from_file(str(sys.argv[1]), num))
+            else:
+                print("Argument non valable")
+        except:
+            print("Argument numérique incorrect")
+
+
 newGame()
