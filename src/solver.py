8b5d7af51d1f453d3a4b7dd933eefefad1c5b1df
AntoinePiron <pro.piron.antoine@gmail.com>
2021-09-27 09:22:14 +0200

    implémentation reduce domain

--- src/solver.py
+++ src/solver.py
@@ -29,8 +29,14 @@ class SudokuSolver:
             self.solutions.append((allPos, val_poss))
 
     def reduce_domains(self, last_i, last_j, last_v):
-        raise NotImplementedError()
-
+        for val in self.solutions:
+        ##On récupère la région de l'elements courant et de l'element precedent
+            region_val = (val[0][0] // 3, val[0][1] // 3)
+            region_last = (last_i // 3, last_j // 3)
+            if last_v in val[1] and (
+                    val[0][0] == last_i or val[0][1] == last_j or (region_val == region_last)):
+                val[1].remove(last_v)
+    
     def commit_one_var(self):
         #On vient parcourir les solutions
         for val in self.solutions:
