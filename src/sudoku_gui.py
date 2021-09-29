03bdb5844d076278d023a6cd19196088f392d7e3
AntoinePiron <pro.piron.antoine@gmail.com>
2021-09-29 16:02:48 +0200

    avancement gui

--- src/sudoku_gui.py
+++ src/sudoku_gui.py
@@ -3,8 +3,22 @@
 
 import easygui
 
-def main():
+#Cette méthode va servir d'écran d'accueil pour le jeu 
+def entry_screen():
+    #On utilise une button box pour récuperer le choix 
+    res = easygui.buttonbox('Bienvenue dans ce jeu de sudoku', 'Sudoku', ('Play', 'Quit'))
+    return res
+
+def game():
     pass
+
+def main():
+    start_game = entry_screen()
+    if start_game == 'Play':
+        print("Début du jeu")
+        game()
+    else: 
+        exit()
 
 if __name__ == "__main__":
     main()
