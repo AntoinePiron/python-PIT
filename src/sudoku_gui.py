19634af68fbd641693a79676ac103453f77b2353
AntoinePiron <pro.piron.antoine@gmail.com>
2021-09-30 08:37:29 +0200

    finition menu

--- src/sudoku_gui.py
+++ src/sudoku_gui.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 #-*-coding: utf8-*-
 import tkinter as tk
-from tkinter.constants import Y
+
+def gameScreen():
+    print("ok")
+
+def which_button(entry, window):
+    #Quelque soit le choix de l'utilisateur on va quitter la fenetre 
+    window.quit()
+    if entry == "Play":
+        gameScreen()
 
 #Cette méthode va servir d'écran d'accueil pour le jeu 
 def entry_screen():
@@ -12,9 +20,11 @@ def entry_screen():
     #Taille de la fenetre
     win.geometry("500x500")
     #Placement d'un bouton play au centre de la fenetre
-    tk.Button(win, text ="Play", width=20, height=2).place(relx=.5, rely=.5, anchor="center")
+    playButton = tk.Button(win, text ="Play", width=20, height=2, command=lambda m="Play": which_button(m,win))
+    playButton.place(relx=.5, rely=.45, anchor="center")
+    quitButton = tk.Button(win, text ="Quit", width=20, height=2, command=lambda m="Quit": which_button(m,win))
+    quitButton.place(relx=.5, rely=.55, anchor="center")
     win.mainloop()
-    return win
 
 def main():
     entry_screen()
