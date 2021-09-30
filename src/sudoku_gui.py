a638fad789b6198c501ccb3cb685aba3c953f841
AntoinePiron <pro.piron.antoine@gmail.com>
2021-09-30 18:24:48 +0200

    finition interface

--- src/sudoku_gui.py
+++ src/sudoku_gui.py
@@ -3,9 +3,18 @@
 import tkinter as tk
 from tkmacosx import Button
 from tkinter.constants import *
-from grid import SudokuGrid
 
-def createGrid(grid):
+from tkmacosx.widgets import button
+from grid import SudokuGrid
+import easygui
+
+def clickCase(window,rowindex,colindex,grid):
+    res = easygui.integerbox("Veuillez rentrer la valeur souhaitée", "Case ("+str(rowindex+1)+","+str(colindex+1)+")", lowerbound=1, upperbound=9)
+    grid.write(rowindex, colindex, res)
+    window.destroy()
+    createGrid(grid)
+
+def createGrid(sudokgrid):
     win = tk.Tk()
     win.title("Suduoku main")
     for rowindex in range (9):
@@ -14,24 +23,23 @@ def createGrid(grid):
                     colour="light blue"
             else:
                 colour="white"
-            x=grid[rowindex][colindex]
+            x=sudokgrid.grid[rowindex][colindex]
             if x==0:
                 colourTxt="red"
             else:
                 colourTxt="black" 
             #Sur macos je suis obligé d'utiliser l'import tkmacosx la ligne pour les autres systemes d'exploitation est commenté    
             #MAC
-            btn=Button(win, width=60, height=60, bg=colour, text=x, fg=colourTxt)  
-            #AUTRE 
-            #btn=tk.Button(win, width=60, height=60, bg=colour, text=x, fg=colourTxt)  
+            btn=Button(win, width=60, height=60, bg=colour, text=x, fg=colourTxt, command=lambda row = rowindex, col = colindex, window = win, grid = sudokgrid: clickCase(window,row,col,grid))
+            #AUTRE,
+            #btn=tk.Button(win, width=3, height=3, bg=colour, text=x, fg=colourTxt, command=lambda row = rowindex, col = colindex, window = win, grid = sudokgrid: clickCase(window,row,col,grid)) 
             btn.grid(row=rowindex, column=colindex, sticky=N+S+E+W)
-            btn.grid(row=rowindex, column=colindex, sticky=N+S+E+W)
-
+            btn.grid(row=rowindex, column=colindex, sticky=N+S+E+W)    
     win.mainloop()
 
 def gameScreen():
     gridToSend = SudokuGrid.from_file("sudoku_db.txt", 1)
-    createGrid(gridToSend.grid)
+    createGrid(gridToSend)
 
 def which_button(entry, window):
     window.destroy()
@@ -47,10 +55,18 @@ def entry_screen():
     #Taille de la fenetre
     win.geometry("500x500")
     #Placement d'un bouton play au centre de la fenetre
-    playButton = tk.Button(win, text ="Play", width=20, height=2, command=lambda m="Play": which_button(m,win))
-    playButton.place(relx=.5, rely=.45, anchor="center")
-    quitButton = tk.Button(win, text ="Quit", width=20, height=2, command=lambda m="Quit": which_button(m,win))
-    quitButton.place(relx=.5, rely=.55, anchor="center")
+    #J'utilise les boutons mac os de nouveau
+
+    #MAC
+    playButton = Button(win, text ="Play", width=300, height=50, command=lambda m="Play": which_button(m,win))
+    quitButton = Button(win, text ="Quit", width=300, height=50, command=lambda m="Quit": which_button(m,win))
+
+    #WINDOWS
+    #layButton = tk.Button(win, text ="Play", width=20, height=5, command=lambda m="Play": which_button(m,win))
+    #quitButton = tk.Button(win, text ="Quit", width=20, height=5, command=lambda m="Quit": which_button(m,win))
+
+    playButton.place(relx=.5, rely=.40, anchor="center")
+    quitButton.place(relx=.5, rely=.60, anchor="center")
     win.mainloop()
 
 if __name__ == "__main__":
