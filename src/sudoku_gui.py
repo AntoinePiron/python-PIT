26a18b155709ba160fbc13717d46ccbc33016f2b
AntoinePiron <pro.piron.antoine@gmail.com>
2021-09-30 08:04:02 +0200

    annulation easygui

--- src/sudoku_gui.py
+++ src/sudoku_gui.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python3
 #-*-coding: utf8-*-
-
-import easygui
+import tkinter
 
 #Cette méthode va servir d'écran d'accueil pour le jeu 
 def entry_screen():
-    #On utilise une button box pour récuperer le choix 
-    res = easygui.buttonbox('Bienvenue dans ce jeu de sudoku', 'Sudoku', ('Play', 'Quit'))
-    return res
-
-def game():
     pass
 
 def main():
-    start_game = entry_screen()
-    if start_game == 'Play':
-        print("Début du jeu")
-        game()
-    else: 
-        exit()
+    pass
 
 if __name__ == "__main__":
     main()
