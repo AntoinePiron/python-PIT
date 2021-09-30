56a61b9c7fa2d4e44ee6626e2eeab744fc375eff
AntoinePiron <pro.piron.antoine@gmail.com>
2021-09-30 08:12:52 +0200

    creation d'une fenetre tkinter

--- src/sudoku_gui.py
+++ src/sudoku_gui.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 #-*-coding: utf8-*-
-import tkinter
+import tkinter as tk
 
 #Cette méthode va servir d'écran d'accueil pour le jeu 
 def entry_screen():
-    pass
+    root = tk.Tk()
+    root.title("Main Menu")
+    root.geometry("500x500")
+    root["bg"] = "coral"
+    root.mainloop()
 
 def main():
+    entry_screen()
     pass
 
 if __name__ == "__main__":
