2384d8e4bfbf92a286c337aec13132bef5b060c0
AntoinePiron <pro.piron.antoine@gmail.com>
2021-09-30 08:21:14 +0200

    meilleur nom de variable et ajout de commentaire

--- src/sudoku_gui.py
+++ src/sudoku_gui.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 #-*-coding: utf8-*-
 import tkinter as tk
+from tkinter.constants import Y
 
 #Cette méthode va servir d'écran d'accueil pour le jeu 
 def entry_screen():
-    root = tk.Tk()
-    root.title("Main Menu")
-    root.geometry("500x500")
-    root["bg"] = "coral"
-    root.mainloop()
+    #Création de la fenetre
+    win = tk.Tk()
+    #Choix du titre de la fenetre
+    win.title("Main Menu")
+    #Taille de la fenetre
+    win.geometry("500x500")
+    #Placement d'un bouton play au centre de la fenetre
+    tk.Button(win, text ="Play", width=20, height=2).place(relx=.5, rely=.5, anchor="center")
+    win.mainloop()
+    return win
 
 def main():
     entry_screen()
